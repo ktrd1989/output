85cd16cddbe87e700c3b857d3d3471ef369bbb8e
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-04 16:42:59 +0900

    修正

--- db/migrate/20200304054857_devise_create_users.rb
+++ db/migrate/20200304054857_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.text :image
 
       ## Recoverable
       t.string   :reset_password_token

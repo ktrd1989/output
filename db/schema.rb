d5f833ed6de9a717b8ab805a7f9f4eefa1cf3101
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-04 17:25:12 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_04_081527) do
-
+ActiveRecord::Schema.define(version: 2020_03_04_082439) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -28,10 +26,10 @@ ActiveRecord::Schema.define(version: 2020_03_04_081527) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["category_id"], name: "index_reports_on_category_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

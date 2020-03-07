037bedb3a89bac6e491c60fd1736d8f94d24b573
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-07 16:45:45 +0900

    新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
 
-  #registrationsで追加したのと同様
-  def after_sign_in_path_for(resource)
-    "/user/#{current_user.id}"
-  end
+  
 
   protected
   

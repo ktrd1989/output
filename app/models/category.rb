6bde43e54fc49716721987dddae8fbda88dfd3fa
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-09 15:20:03 +0900

    カテゴリー

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :reports
+
+  has_ancestry
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+book = Category.create(:name=>"書籍")
+
+book_1 = book.children.create(:name=>"趣味・実用")
+book_2 = book.children.create(:name=>"ビジネス・経済")
+
+
+
